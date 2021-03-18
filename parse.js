d6b8e3ad42e84d61b585a1a79722aa6da5cf69bb
JakeGuy11 <Jake_Guy_11@protonmail.ch>
2021-03-17 22:30:36 -0400

    added reddit feed parsing

--- parse.js
+++ parse.js
@@ -1,1 +1,19 @@
-console.log("Here is where the js stuff will be done");
+console.log("Starting plugin!");
+
+var feedNodeList = document.querySelectorAll("div.rpBJOHq2PR60pnwJlUyP0").item(0).childNodes;
+console.log(feedNodeList);
+
+for(var i = 0; i < feedNodeList.length; i++) {
+
+	try{
+		var currentNode = feedNodeList.item(i);
+
+		imageURL = currentNode.childNodes.item(0).childNodes.item(0).childNodes.item(1).childNodes.item(2).childNodes.item(0).childNodes.item(1).childNodes.item(0).childNodes.item(0).childNodes.item(0).childNodes.item(0).childNodes.item(0).attributes.item(2).nodeValue;
+
+		console.log(imageURL);
+	}
+	catch (err) {
+		//console.log("Error encountered in parsing");
+	}
+	
+}
