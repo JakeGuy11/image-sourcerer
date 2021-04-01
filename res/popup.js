48c75be54626113231054b97a4a5f80242073fea
JakeGuy11 <Jake_Guy_11@protonmail.ch>
2021-04-01 12:07:18 -0400

    added export feature, updated popup

---
+++ res/popup.js
@@ -0,0 +1,28 @@
+notifySignal({ "intent": "relay", "content": "=====================" });
+notifySignal({ "intent": "relay", "content": "Starting Popup Script" });
+notifySignal({ "intent": "relay", "content": "=====================" });
+
+function openGithubPage(){
+	chrome.tabs.create({
+		active: true,
+		url:  'https://github.com/JakeGuy11/image-archive'
+		}, null);
+}
+
+function openGithubTemplate(){
+	chrome.tabs.create({
+		active: true,
+		url:  'https://github.com/JakeGuy11/image-sourcerer/blob/main/page-scripts/_template.js'
+		}, null);
+}
+
+function downloadLog() {
+	notifySignal({ "intent": "relay", "content": "Sending download log flag" });
+	notifySignal({ "intent": "download_log" });
+}
+
+function notifySignal(msg) {
+	msg.sender = "popup.js";
+	if(!("intent" in msg)) msg.intent = "undefined_intent";
+	chrome.runtime.sendMessage(msg);
+}
