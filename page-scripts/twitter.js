6e026778ef3d829b1071982b3e521b084765cd37
Jake <Jake_Guy_11@protonmail.ch>
2021-07-08 22:54:57 -0400

    rewrote and fixed download function

--- page-scripts/twitter.js
+++ page-scripts/twitter.js
@@ -3,12 +3,33 @@ notifySignal({ "intent": "relay", "content": "=======================" });
 notifySignal({ "intent": "relay", "content": "Starting Twitter Script" });
 notifySignal({ "intent": "relay", "content": "=======================" });
 
-function startDownload(url, pageLink, author) {
-    //try {
-    //    likeButton.click();
-    //} catch (e) {
-    //    console.log("Could not like post");
-    //}
+function startDownload(images, pageLink, author) 
+{
+    // This will hold the actual url to download
+    let url = "";
+    
+    // Make sure the images are in order
+    let ordered_images = [];
+    switch (images.length)
+    {
+        case 1: ordered_images = images;
+        case 2: ordered_images = images;
+        case 3: ordered_images = images;
+        case 4: ordered_images[0] = images[0]; ordered_images[1] = posts[2]; ordered_images[2] = posts[1]; ordered_images[3] = posts[3];
+        default: alert("Failed to parse image list! Please report this to the Image Sourcerer github page."); return;
+    }
+
+    // If there are >1 images, prompt the user for which to download
+    if (ordered_images.length == 1) url = ordered_images[0];
+    else
+    {
+        let req_image = prompt("Enter the number of the post you would like to download (left to right, top to bottom):","");
+        let req_index = parseInt(req_image, 10);
+        if (isNaN(req_index)) alert("That is not a valid image index!"); return;
+        url = ordered_images[req_index - 1];
+    }
+
+    // Get the proper extention
     var extention = "";
     url = url.split("name=")[0] + "name=large";
     if(url.includes("format=jpg")){
@@ -21,89 +42,16 @@ function startDownload(url, pageLink, author) {
         notifySignal({ "intent": "relay", "content": "Extention could not be found." });
         return;
     }
-    var saveName = prompt("Enter the path (relative to ~/Downloads/Image-Sourcerer/) and filename you would like to save the image under","");
+
+    // Find where to save the image
+    var saveName = prompt("Enter the path (relative to ~/<Download path>/Image-Sourcerer/) and filename you would like to save the image under","");
     if(saveName.includes("..")){
         alert("Your saveName cannot include '..'");
         return;
     }
+
+    // Send the signal to download
     notifySignal({ "intent": "queue_download", "target_url": url, "save_name": "Image-Sourcerer/" + saveName, "ext": extention, "post_src": pageLink, "op": author, "page": "https://twitter.com/" });
-}
-
-function handleMultiPost(aList, linkToPost, author, likeButton){
-    var desiredSlide = prompt("Enter the number of the post you would like to download (left to right, top to bottom):","");
-    if(desiredSlide === null) return;
-    var desiredSlideNum = parseInt(desiredSlide);
-    if(desiredSlideNum === parseInt(desiredSlide, 10)){
-        var imageURL = "";
-        try {
-            var imagesList = aList.getElementsByTagName("img");
-            switch(imagesList.length) {
-                case 2:
-                    switch (desiredSlideNum) {
-                        case 1:
-                            imageURL = imagesList[0].src;
-                            break;
-                        case 2:
-                            imageURL = imagesList[1].src;
-                            break;
-                        default:
-                            throw new Error("Index out of bounds");
-                    }
-                    break;
-                case 3:
-                    switch (desiredSlideNum) {
-                        case 1:
-                            imageURL = imagesList[0].src;
-                            break;
-                        case 2:
-                            imageURL = imagesList[1].src;
-                            break;
-                        case 3:
-                            imageURL = imagesList[2].src;
-                            break;
-                        default:
-                            throw new Error("Index out of bounds");
-                    }
-                    break;
-                case 4:
-                    switch (desiredSlideNum) {
-                        case 1:
-                            imageURL = imagesList[0].src;
-                            break;
-                        case 2:
-                            imageURL = imagesList[2].src;
-                            break;
-                        case 3:
-                            imageURL = imagesList[1].src;
-                            break;
-                        case 4:
-                            imageURL = imagesList[3].src;
-                            break;
-                        default:
-                            throw new Error("Index out of bounds");
-                    }
-                    break;
-                default:
-                    throw new Error("Out of range");
-            }
-            startDownload(imageURL, linkToPost, author, likeButton);
-        } catch (err) {
-            alert("The image at that index is not available");
-            console.log(err);
-        }
-    }else{
-        alert("The slide value you entered in not a number");
-    }
-}
-
-function feedIsProfile() {
-    try {
-        var indicator = document.getElementById("react-root").childNodes.item(0).childNodes.item(0).childNodes.item(2).childNodes.item(3).childNodes.item(0).childNodes.item(0).childNodes.item(0).childNodes.item(0).childNodes.item(0).childNodes.item(1).childNodes.item(0).childNodes.item(0).childNodes.item(0).childNodes.item(1).childNodes.item(0).childNodes.item(0).childNodes.item(0).childNodes.item(1).childNodes.item(0).childNodes.item(1).tagName.toLowerCase();
-        if (indicator == "img") return true;
-        else return false;
-    } catch (e) {
-        return false;
-    }
 }
 
 function get_feed_list(feed_root)
@@ -176,7 +124,13 @@ function refreshNodes() {
         if (all_op_tags.length < 1) op = "UNKNOWN";
         else op = all_op_tags[all_op_tags.length - 1].innerText;
 
-        console.log(op + " posted " + interested_images.join(", "));
+        // Get the link
+        let all_link_tags = current_node.getElementsByClassName("css-4rbku5 css-18t94o4 css-901oao r-9ilb82 r-1loqt21 r-1q142lx r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-3s2u2q r-qvutc0");
+        let link = "";
+        if (all_link_tags.length < 1) link = "UNKNOWN";
+        else link = all_link_tags[all_link_tags.length - 1].href;
+
+        console.log(op + " posted " + interested_images.join(", ") + " at " + link);
 
         // Bind everything we've collected to a download function
     }
